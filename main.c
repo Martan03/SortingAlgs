011393c59f4d448eb37068b952ca57c65102d003
Martan03 <martinslezak03@gmail.com>
2022-11-29 22:39:10 +0100

    Add Selection sort

--- main.c
+++ main.c
@@ -10,6 +10,11 @@ void Swap(double *x, double *y);
 /// @param len length of the array
 void BubbleSort(double **nums, int len);
 
+/// @brief Sorts double array using Selection sort
+/// @param nums double array
+/// @param len length of the array
+void SelectSort(double **nums, int len);
+
 /// @brief Prints double array
 /// @param nums double array
 /// @param len length of the array
@@ -20,7 +25,8 @@ int main()
     double n[] = { 10, 8, 2, 7, 4, 3, 6, 1, 9, 5 };
     double *nums = n;
     int len = 10;
-    BubbleSort(&nums, len);
+    //BubbleSort(&nums, len);
+    SelectSort(&nums, len);
     PrintNums(nums, len);
 }
 
@@ -38,6 +44,21 @@ void BubbleSort(double **nums, int len)
         for (int j = i + 1; j < len; ++j)
             if (n[i] > n[j])
                 Swap(&n[i], &n[j]);
+}
+
+void SelectSort(double **nums, int len)
+{
+    double *n = *nums;
+    for (int min, i = 0; i < len; ++i)
+    {
+        min = i;
+        for (int j = i + 1; j < len; ++j)
+            if (n[min] > n[j])
+                min = j;
+
+        if (min != i)
+            Swap(&n[i], &n[min]);
+    }
 }
 
 void PrintNums(double *nums, int len)
