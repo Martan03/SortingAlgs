746f8d1887c314923671bdccb04d9ac898d9215c
Martan03 <martinslezak03@gmail.com>
2022-11-30 11:13:25 +0100

    Add Insertion sort

--- main.c
+++ main.c
@@ -8,12 +8,17 @@ void Swap(double *x, double *y);
 /// @brief Sorts double array using Bubble sort
 /// @param nums double array
 /// @param len length of the array
-void BubbleSort(double **nums, int len);
+void BubbleSort(double *nums, int len);
 
 /// @brief Sorts double array using Selection sort
 /// @param nums double array
 /// @param len length of the array
-void SelectSort(double **nums, int len);
+void SelectSort(double *nums, int len);
+
+/// @brief Sorts double array using Insertion sort
+/// @param nums double array
+/// @param len length of the array
+void InsertSort(double *nums, int len);
 
 /// @brief Prints double array
 /// @param nums double array
@@ -22,11 +27,11 @@ void PrintNums(double *nums, int len);
 
 int main()
 {
-    double n[] = { 10, 8, 2, 7, 4, 3, 6, 1, 9, 5 };
-    double *nums = n;
+    double nums[] = { 10, 8, 2, 7, 4, 3, 6, 1, 9, 5 };
     int len = 10;
-    //BubbleSort(&nums, len);
-    SelectSort(&nums, len);
+    //BubbleSort(nums, len);
+    //SelectSort(nums, len);
+    InsertSort(nums, len);
     PrintNums(nums, len);
 }
 
@@ -37,27 +42,37 @@ void Swap(double *x, double *y)
     *y = temp;
 }
 
-void BubbleSort(double **nums, int len)
+void BubbleSort(double *nums, int len)
 {
-    double *n = *nums;
     for (int i = 0; i < len; ++i)
         for (int j = i + 1; j < len; ++j)
-            if (n[i] > n[j])
-                Swap(&n[i], &n[j]);
+            if (nums[i] > nums[j])
+                Swap(&nums[i], &nums[j]);
 }
 
-void SelectSort(double **nums, int len)
+void SelectSort(double *nums, int len)
 {
-    double *n = *nums;
     for (int min, i = 0; i < len; ++i)
     {
         min = i;
         for (int j = i + 1; j < len; ++j)
-            if (n[min] > n[j])
+            if (nums[min] > nums[j])
                 min = j;
 
         if (min != i)
-            Swap(&n[i], &n[min]);
+            Swap(&nums[i], &nums[min]);
+    }
+}
+
+void InsertSort(double *nums, int len)
+{
+    for (int j, k, i = 0; i < len; ++i)
+    {
+        for (j = i + 1; j < len && nums[j - 1] < nums[j]; ++j);
+        if (j == len)
+            break;
+        for (k = j - 1; k >= 0 && nums[k + 1] < nums[k]; --k)
+            Swap(&nums[k + 1], &nums[k]);
     }
 }
 
