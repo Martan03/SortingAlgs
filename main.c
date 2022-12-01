846b1a03f82ba4c00e841aaba925b158a6f58631
Martan03 <martinslezak03@gmail.com>
2022-12-01 22:30:08 +0100

    Add Merge sort

--- main.c
+++ main.c
@@ -20,6 +20,13 @@ void SelectSort(double *nums, int len);
 /// @param len length of the array
 void InsertSort(double *nums, int len);
 
+/// @brief Sorts double array using Merge sort
+/// @param nums double array
+/// @param len length of the array
+void MergeSort(double *nums, int len);
+
+void _MergeSort(double *nums, int left, int right);
+
 /// @brief Prints double array
 /// @param nums double array
 /// @param len length of the array
@@ -31,7 +38,8 @@ int main()
     int len = 10;
     //BubbleSort(nums, len);
     //SelectSort(nums, len);
-    InsertSort(nums, len);
+    //InsertSort(nums, len);
+    MergeSort(nums, len);
     PrintNums(nums, len);
 }
 
@@ -74,6 +82,45 @@ void InsertSort(double *nums, int len)
         for (k = j - 1; k >= 0 && nums[k + 1] < nums[k]; --k)
             Swap(&nums[k + 1], &nums[k]);
     }
+}
+
+void MergeSort(double *nums, int len)
+{
+    if (len <= 0)
+        return;
+    _MergeSort(nums, 0, len - 1);
+}
+
+void _MergeSort(double *nums, int left, int right)
+{
+    if (left >= right)
+        return;
+    
+    int middle = (left + right) / 2;
+    _MergeSort(nums, left, middle);
+    _MergeSort(nums, middle + 1, right);
+
+    int tempLen = right - left + 1;
+    double temp[tempLen];
+    int l, r, k;
+    for (l = left, r = middle + 1, k = 0; k < tempLen && l <= middle && r <= right; ++k)
+    {
+        if (nums[l] < nums[r])
+            temp[k] = nums[l++];
+        else
+            temp[k] = nums[r++];
+    }
+
+    for (; l <= middle; ++l)
+        temp[k++] = nums[l];
+
+    for (; r <= right; ++r)
+        temp[k++] = nums[r];
+
+    for (int i = 0; i < tempLen; ++i)
+        nums[left + i] = temp[i];
+    
+    return;
 }
 
 void PrintNums(double *nums, int len)
