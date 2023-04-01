56d321a41bbb12e10695c06bf3b72fec5364f0b7
Martan03 <martinslezak03@gmail.com>
2023-04-01 10:56:07 +0200

    Add C qsort test

--- src/main.c
+++ src/main.c
@@ -3,9 +3,28 @@
 #include <time.h>
 #include "sorts.h"
 
+/// @brief Generates array with random numbers
+/// @param len length of array to be generated
+/// @param t time to make randomization based on
+/// @return generated array
 double* random_nums(int len, time_t t);
 
+/// @brief Tests sort algorithm
+/// @param sort sort function
+/// @param t time to base randomization on
+/// @param len length of the array to be generated
 void test_sort(void (*sort)(double* nums, int len), time_t t, int len);
+
+/// @brief Tests C qsort algorithm
+/// @param t time to base randomization on
+/// @param len length of the array to be generated
+void test_qsort(time_t t, int len);
+
+/// @brief Compares two numbers
+/// @param a number
+/// @param b number
+/// @return 1 if a is greater, 0 if numbers are equal, -1 if a is less
+int compare(const void* a, const void* b);
 
 int main() {
     time_t t;
@@ -33,6 +52,9 @@ int main() {
     printf("%30s", "Merge sort: ");
     test_sort(&merge_sort, t, len);
 
+    printf("%30s", "C qsort: ");
+    test_qsort(t, len);
+
     return 0;
 }
 
@@ -59,4 +81,25 @@ void test_sort(void (*sort)(double* nums, int len), time_t t, int len) {
     printf("time=%.3g\n", (double)(clock() - start) / CLOCKS_PER_SEC);
 
     free(nums);
+}
+
+void test_qsort(time_t t, int len) {
+    srand(t);
+
+    double* nums = random_nums(len, t);
+    if (!nums)
+        return;
+
+    clock_t start = clock();
+    qsort(nums, len, sizeof(double), compare);
+    printf("time=%.3g\n", (double)(clock() - start) / CLOCKS_PER_SEC);
+
+    free(nums);
+}
+
+int compare(const void* a, const void* b) {
+    double da = *((double*) a);
+    double db = *((double*) b);
+
+    return (da > db) - (db > da);
 }
